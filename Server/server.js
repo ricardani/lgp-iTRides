673f75a002018308407e5122960da707cdbaf850
Ricardo Silva <ricardani@gmail.com>
2015-04-23 15:21:07 +0100

    added cross domain to server

--- Server/server.js
+++ Server/server.js
@@ -14,6 +14,30 @@ var app = express();
 
 app.use(bodyParser());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.header('Access-Control-Allow-Credentials', true);
+    
+    if (req.method === 'OPTIONS') {
+        // Bypass browser's CORS options requests
+        res.send(200);
+    } else {
+        // Pass to next layer of middleware
+        next();
+    }
+});
+
 mongoose.connect('mongodb://itrides:itridesadmin1@ds037581.mongolab.com:37581/itrides');
 
 //load all files in models dir
