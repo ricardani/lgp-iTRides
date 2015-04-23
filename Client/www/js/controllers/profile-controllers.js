dc8489d295fd89d5de348be2cdb3ca27a8cf90c7
Joao Vieira de Castro <manueljoaocastro@gmail.com>
2015-04-23 19:38:08 +0100

    ListRides implemented

--- Client/www/js/controllers/profile-controllers.js
+++ Client/www/js/controllers/profile-controllers.js
@@ -8,7 +8,22 @@ angular.module('iTRides.profileControllers', [])
             name: 'Raquel Gomes', photo: 'https://lh3.googleusercontent.com/-IYWkoctkgdo/AAAAAAAAAAI/AAAAAAAAAAA/v3okpkLGc_4/s46-c-k-no/photo.jpg', email: 'raquel_gomes@itgrow.com', contact: '913 637 973', rating: 3
         };
 
-        $scope.defaultRides = [];
+        $scope.defaultRides = [{
+        	id:0,
+        	name: 'Trabalho',
+        	day: 'Segunda Feira',
+        	start_time: '07:30'
+        }, {
+        	id:1,
+        	name: 'Casa',
+        	day: 'Segunda Feira',
+        	start_time: '18:30'
+        }, {
+        	id:2,
+        	name: 'Trabalho',
+        	day: 'Terça Feira',
+        	start_time: '08:00'
+        }];
 
         $scope.number = 5-$scope.user.rating;
 		$scope.getNumber = function(num) {
