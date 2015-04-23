14ecb0601ae57a78057ba7e6f81f5344cbe96b7f
ster1177 <ster1177@gmail.com>
2015-04-23 18:31:39 +0100

    Pesquisa por datas (so buscar valores)

--- Client/www/js/controllers/search-controllers.js
+++ Client/www/js/controllers/search-controllers.js
@@ -4,15 +4,27 @@ angular.module('iTRides.searchControllers', [])
 	$scope.selectedRideType = 0;
 	$scope.startDistrito = '';
 	$scope.startConcelho = '';
-	$scope.startFreguesia = '';
-})
 
-.controller('RideTypeCtrl', function($scope) {
+	
+	$scope.date='';
+
+	$scope.dateChange=function(val){
+	console.log("on-change",val);
+   };
+
 	$scope.collection = ["Casa>Trabalho", "Trabalho>Casa", "Ocasional"];
 
 	$scope.itemClicked = function ($index) {
 		$scope.selectedRideType = $index;
 	};
+
+
+})
+
+ 	
+
+.controller('RideTypeCtrl', function($scope) {
+	
 })
 
 .controller('StartAddressCtrl', function($scope, $ionicSideMenuDelegate) {
